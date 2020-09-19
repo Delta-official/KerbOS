33848b53d10d5b9576ed1843602876199af65961
Darkuss420 <64544951+Darkuss420@users.noreply.github.com>
2020-09-19 15:54:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,10 +66,10 @@ client.on('message', message =>{
     if(command === 'help'){
         function deleteMes() {
             if(message.author.user) return;
-            if(message.content === '```diff\nLoading information...```') message.delete();
+            if(message.content === '```diff\n- Loading information...\n```') message.delete();
         }
 
-        message.channel.send('```diff\nLoading information...```')
+        message.channel.send('```diff\n- Loading information...\n```')
         setTimeout(deleteMes,3000);
         const exam1Embed = new Discord.MessageEmbed()
         .setColor('#008000')
