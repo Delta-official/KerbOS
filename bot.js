ee5c3f8ea7933d15f32b4b5f12267a680e9045e7
Darkuss420 <64544951+Darkuss420@users.noreply.github.com>
2020-10-18 08:06:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -313,14 +313,14 @@ client.on('message', async(message) => {
         }
         if (command === 'loungepass') {
             if(message.guild.id === STRATZ_SERVER_ID) {
-                if(member.roles.cache.has(PATREON_ID, PATREONPLUS_ID)) {
+                if(message.member.roles.cache.has(PATREON_ID, PATREONPLUS_ID)) {
                     member.roles.add(Lpass)
                     message.channel.send('*Welcome to the Stratzen Lounge!*');
             }
             } else {
             message.channel.send("*You need to have a Patreon or Patreon+ role in order to get Lounge Pass!*")
             }
-                if(member.roles.roles.cache.has(LP_ID)) {
+                if(message.member.roles.roles.cache.has(LP_ID)) {
                     message.channel.send("*You already have Lounge Pass!")
                 }
         }
