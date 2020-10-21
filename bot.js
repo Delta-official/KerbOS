a6050592bb39f17cb9c20e6c32e4c1c8e4b703ff
Darkuss420 <64544951+Darkuss420@users.noreply.github.com>
2020-10-21 14:22:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,7 +60,7 @@ let canNotifyStreaming = true;
 
 client.on("presenceUpdate", (oldPresence, newPresence) => {
     if(newPresence.user.id === LORD_ID) {
-    if(newPresence.guild.id === NEWS_ID.guild.id) {
+    if(newPresence.guild.id === STRATZ_SERVER_ID) {
     if (!newPresence.activities) return false;
     newPresence.activities.forEach(activity => {
         if (activity.type == "STREAMING") {
