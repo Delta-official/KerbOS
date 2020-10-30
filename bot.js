ea1d8903ff0728771a1755e36e75e75052d6031a
Darkuss420 <64544951+Darkuss420@users.noreply.github.com>
2020-10-30 18:43:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,12 +34,13 @@ for(const file of OwnerCommands) {
     client.commands.set(command0.name, command0)
 }
 
+var today = new Date();
+var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+'/ Current time:'+time;
+
 client.on('ready', () => {
     console.log('Booting up KerbOS.V1...\nWelcome User01, please enter password:\n Bread_is_cool12345');
-    var today = new Date();
-    var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var dateTime = date+'/ Current time:'+time;
     client.users.cache.get(OWNER_ID).send(`Current date: ${dateTime}`);
     //  _  __         _      ____   _____      
     // | |/ /        | |    / __ \ / ____|  
