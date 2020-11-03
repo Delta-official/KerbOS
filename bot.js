3244fa2babe29497cbe006b35d2a274fb9716bb7
Darkuss420 <64544951+Darkuss420@users.noreply.github.com>
2020-11-03 09:04:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,7 +51,7 @@ client.on('ready', () => {
 let blocker = {}
 
 /*client.on("roleUpdate", ())*/
-
+// bot
 // Our entire code in a nutshell:
 // OOPSIE WOOPSIE!! UwU We made a fucky wucky!! 
 // A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this
